0fba94cd4a3af59dacb9aa26f0a3019df3e15814
Phan Văn Chinh <phanchinhnd1@gmail.com>
2024-03-27 23:23:09 +0700

    update

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -2,19 +2,13 @@ package runner;
 
 import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
 import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("/features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/",
+        plugin = {"pretty", "json:target/cucumber.json"},
+        glue = {"steps"})
 public class Runner {
 }
